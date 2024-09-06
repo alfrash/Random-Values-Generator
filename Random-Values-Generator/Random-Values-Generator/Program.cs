b96268265fd3a916b4b4eb1adf53869f86075d6e
alfrash <ahmed.alfrash@icloud.com>
2024-09-06 23:45:21 +0300

    print options

---
+++ Random-Values-Generator/Random-Values-Generator/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Select an option");
+Console.WriteLine("[1] Generate random number\n[2] Generate random string");
+
