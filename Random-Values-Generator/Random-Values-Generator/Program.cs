2940b3749aa354210cc8e3e84ff9ea70dc559b9b
alfrash <ahmed.alfrash@icloud.com>
2024-09-07 00:15:20 +0300

    Create GenerateRandomNumber

--- Random-Values-Generator/Random-Values-Generator/Program.cs
+++ Random-Values-Generator/Random-Values-Generator/Program.cs
@@ -1,4 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Select an option");
 Console.WriteLine("[1] Generate random number\n[2] Generate random string");
+var selectedOption = Console.ReadLine();
+if (selectedOption == "1") {
+    GenerateRandomNumber();
+}
 
+void GenerateRandomNumber() {
+    var rnd = new Random();
+    var value = rnd.Next(10_000, 100_000);
+    Console.WriteLine("Random number = " + value);
+}
